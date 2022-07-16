120ea002c47c45dd00c495b021ec246d68ef6ac4
Rolands Jankovskis <Rolands.jankovskis@gmail.com>
2022-07-16 23:32:17 +0300

    Added Args for public read of uploaded file
    • Added Args for public read of uploaded file in actions.yaml

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,34 +16,10 @@ resource "aws_s3_bucket" "s3Bucket" {
 }
 
 resource "aws_s3_bucket_acl" "s3Bucket" {
-    bucket = "dev-rj-assets-bucket"
-    access_control_policy {
-    grant {
-      grantee {
-        id   = "349933e16c8a438b0acc75ce44fac2035f8df75532ffcf600ff74a437cf8e648"
-        type = "CanonicalUser"
-      }
-      permission = "FULL_CONTROL"
-    }
-      grant {
-      grantee {
-        type = "Group"
-        uri = "http://acs.amazonaws.com/groups/global/AllUsers"
-      }
-      permission = "READ_ACP"
-      }
-            grant {
-      grantee {
-        type = "Group"
-        uri = "http://acs.amazonaws.com/groups/global/AllUsers"
-      }
-      permission = "READ"
-      }
-      owner {
-        id = "349933e16c8a438b0acc75ce44fac2035f8df75532ffcf600ff74a437cf8e648"
-      }
+     bucket = "rj-assets-bucket"
+     acl       = "public-read"
 }
-}
+
 resource "aws_s3_bucket_website_configuration" "s3Bucket" {
     bucket = "dev-rj-assets-bucket"
     index_document {
