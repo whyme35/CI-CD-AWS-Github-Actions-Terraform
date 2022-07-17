082a6d0dca60c094b486b9a9fb227893f0a02f82
Rolands Jankovskis <Rolands.jankovskis@gmail.com>
2022-07-17 23:30:48 +0300

    Setup Terraform to upload assets to S3
    * Setup Terraform to upload assets to S3 and removed S3 sync from actions.yml to improve deployment speed

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,14 +10,8 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-
 resource "aws_s3_bucket" "s3Bucket" {
      bucket = "rj-assets-bucket"
-}
-
-resource "aws_s3_bucket_acl" "s3Bucket" {
-     bucket = "rj-assets-bucket"
-     acl       = "public-read"
 }
 
 resource "aws_s3_bucket_website_configuration" "s3Bucket" {
@@ -25,4 +19,17 @@ resource "aws_s3_bucket_website_configuration" "s3Bucket" {
     index_document {
         suffix = "index.html"
     } 
+}
+
+resource "aws_s3_object" "s3Bucket_object" {
+depends_on = [
+  aws_s3_bucket.s3Bucket
+]
+bucket = "rj-assets-bucket"
+for_each = fileset("../assets/", "*")
+key = "${each.value}"
+source = "../assets/${each.value}"
+source_hash = filemd5("../assets/${each.value}")
+content_type = "text/html"
+acl = "public-read"
 }
