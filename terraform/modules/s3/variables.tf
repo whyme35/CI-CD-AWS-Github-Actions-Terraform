e5692952e518f2de816a51b4743b86952a2c3972
Rolands Jankovskis <72339907+whyme35@users.noreply.github.com>
2022-07-20 21:02:31 +0300

    Setup terraform in modules and created variable files (#8)
    • Setup terraform in modules and created variable files

---
+++ terraform/modules/s3/variables.tf
@@ -0,0 +1,7 @@
+variable "region" {
+  description = "The region to launch the project"
+}
+
+variable "webpage_assets_bucket" {
+  description = "The bucket used to store Webpage assets"
+}
---
+++ terraform/terraform.tfvars
@@ -0,0 +1,2 @@
+    region="us-east-1"
+    webpage_assets_bucket = "rj-assets-bucket"
